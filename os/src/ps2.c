722bd79cc83b1b4c538161daa928018061d5060c
Michele Balistreri <michele@bitgamma.com>
2021-11-01 20:30:04 +0300

    fix p2 init

--- os/src/ps2.c
+++ os/src/ps2.c
@@ -2,7 +2,7 @@
 #include <port.h>
 #include <stdbool.h>
 
-#define WAIT_CYCLES 5
+#define WAIT_CYCLES 10
 #define KBD_RETRY 3
 #define KBD_INVALID 0xffff
 
@@ -26,10 +26,15 @@ static bool ps2_wait_full_output() {
   return false;
 }
 
-static inline void ps2_send_cmd_data(uint8_t cmd, uint8_t data) {
+static inline void ps2_send_cmd(uint8_t cmd) {
   outb(PS2_CMD_PORT, cmd);
   ps2_wait_empty_input();
+}
+
+static inline void ps2_send_cmd_data(uint8_t cmd, uint8_t data) {
+  ps2_send_cmd(cmd);
   outb(PS2_DATA_PORT, data);
+  ps2_wait_empty_input();
 }
 
 static inline uint8_t ps2_get_cmd_data(uint8_t cmd) {
@@ -48,7 +53,7 @@ static bool ps2_kbd_cmd(uint8_t cmd) {
       return false;
     }
 
-    if (inb(PS2_DATA_PORT) & 0xFA) {
+    if (inb(PS2_DATA_PORT) == 0xFA) {
       return true;
     }
   }
@@ -57,20 +62,8 @@ static bool ps2_kbd_cmd(uint8_t cmd) {
 }
 
 static bool ps2_kbd_set_data(uint8_t cmd, uint8_t data) {
-  uint8_t retry = KBD_RETRY;
-
-  while(retry--) {
-    outb(PS2_DATA_PORT, cmd);
-    ps2_wait_empty_input();
-    outb(PS2_DATA_PORT, data);
-
-    if (!ps2_wait_full_output()) {
-      return false;
-    }
-
-    if (inb(PS2_DATA_PORT) & 0xFA) {
-      return true;
-    }
+  if (ps2_kbd_cmd(cmd)) {
+    return ps2_kbd_cmd(data);
   }
 
   return false;
@@ -98,8 +91,8 @@ static void ps2_kbd_init() {
 }
 
 void ps2_init() {
-  outb(PS2_CMD_PORT, PS2_DISABLE_PORT1);
-  outb(PS2_CMD_PORT, PS2_DISABLE_PORT2);
+  ps2_send_cmd(PS2_DISABLE_PORT1);
+  ps2_send_cmd(PS2_DISABLE_PORT2);
 
   inb(PS2_DATA_PORT);
 
@@ -111,14 +104,14 @@ void ps2_init() {
 
   if (ps2_get_cmd_data(PS2_TEST_PORT1) != 0x00) { /*do something*/ }
 
-  outb(PS2_CMD_PORT, PS2_ENABLE_PORT1);
   ps2_config |= (1 << PS2_CCB_PORT1_INT);
   ps2_send_cmd_data(PS2_WRITE_CONFIG, ps2_config);
+
+  ps2_send_cmd(PS2_ENABLE_PORT1);
 
   ps2_kbd_init();
 }
 
 void ps2_reboot() {
-  ps2_wait_empty_input();
-  outb(PS2_CMD_PORT, PS2_REBOOT);
+  ps2_send_cmd(PS2_REBOOT);
 }
