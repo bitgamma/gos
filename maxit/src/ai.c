94c29e17ea1522b522f85e55ac431cd73356cdbc
Ksenia Balistreri <ksenia@bitgamma.com>
2021-11-06 19:03:05 +0300

    typo

--- maxit/src/ai.c
+++ maxit/src/ai.c
@@ -26,7 +26,7 @@ uint8_t mxt_ai_get_depth(mxt_maxit_t* maxit) {
   return maxit->game.level + 1;
 }
 
-int16_t mxt_ai_minmaxi(mxt_maxit_t* maxit, uint8_t row_col, int16_t score, uint8_t depth, int16_t alpha, int16_t beta, bool maximize, uint8_t* max_row) {
+int16_t mxt_ai_minmax(mxt_maxit_t* maxit, uint8_t row_col, int16_t score, uint8_t depth, int16_t alpha, int16_t beta, bool maximize, uint8_t* max_row) {
   if (depth == 0) {
     return score;
   }
@@ -40,7 +40,7 @@ int16_t mxt_ai_minmaxi(mxt_maxit_t* maxit, uint8_t row_col, int16_t score, uint8
       }
 
       ba_clear(_moves[i], row_col);    
-      int16_t t = mxt_ai_minmaxi(maxit, i, score + maxit->game.board.board[i][row_col], depth - 1, alpha, beta, false, max_row);
+      int16_t t = mxt_ai_minmax(maxit, i, score + maxit->game.board.board[i][row_col], depth - 1, alpha, beta, false, max_row);
       if (t > val) {
         val = t;
         *max_row = i;
@@ -61,7 +61,7 @@ int16_t mxt_ai_minmaxi(mxt_maxit_t* maxit, uint8_t row_col, int16_t score, uint8
         continue;
       }
       ba_clear(_moves[row_col], i);
-      val = min(val, mxt_ai_minmaxi(maxit, i, score - maxit->game.board.board[row_col][i], depth - 1, alpha, beta, true, max_row));
+      val = min(val, mxt_ai_minmax(maxit, i, score - maxit->game.board.board[row_col][i], depth - 1, alpha, beta, true, max_row));
       ba_set(_moves[row_col], i);
       beta = min(beta, val);
       if (val <= alpha) {
@@ -75,6 +75,6 @@ int16_t mxt_ai_minmaxi(mxt_maxit_t* maxit, uint8_t row_col, int16_t score, uint8
 
 void mxt_ai_move(mxt_maxit_t* maxit) {
   ba_clear(_moves[maxit->game.board.cursor_row], maxit->game.board.cursor_column);
-  mxt_ai_minmaxi(maxit, maxit->game.board.cursor_column, 0, mxt_ai_get_depth(maxit), MINF, PINF, true, &maxit->game.board.cursor_row);
+  mxt_ai_minmax(maxit, maxit->game.board.cursor_column, 0, mxt_ai_get_depth(maxit), MINF, PINF, true, &maxit->game.board.cursor_row);
   ba_clear(_moves[maxit->game.board.cursor_row], maxit->game.board.cursor_column);
 }
