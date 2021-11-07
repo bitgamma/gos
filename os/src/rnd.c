28cb74e1bf262545dc4808f07e82ba6765ba92b0
Michele Balistreri <michele@bitgamma.com>
2021-11-07 14:38:11 +0300

    improve rnd

--- os/src/rnd.c
+++ os/src/rnd.c
@@ -15,7 +15,7 @@ void rnd_seed(uint32_t s0, uint32_t s1) {
 uint32_t rnd_next(void) {
 	const uint32_t s0 = s[0];
 	uint32_t s1 = s[1];
-	const uint32_t result = s0 * 0x9E3779BB;
+	const uint32_t result = rotl(s0 * 0x9E3779BB, 5) * 5;
 
 	s1 ^= s0;
 	s[0] = rotl(s0, 26) ^ s1 ^ (s1 << 9); // a, b
