c26ac10c4bd634e5fc39599520b548d9a94cb299
Michele Balistreri <michele@bitgamma.com>
2021-11-13 14:19:09 +0300

    fix bar reading

--- os/src/ac97.c
+++ os/src/ac97.c
@@ -11,7 +11,6 @@
 #define AC97_NABM_LVE 0x15
 #define AC97_NABM_TS 0x16
 #define AC97_NABM_TC 0x1b
-
 #define AC97_NABM_GCR 0x2c
 
 #define AC97_RESET 0x02
@@ -42,21 +41,22 @@ bool ac97_init() {
   }
 
   pci_config_or(AC97_DEV, PCI_HEADER_STATUS_COMMAND, (PCI_COMMAND_BUSMASTER | PCI_COMMAND_PIO));
-  _nam_port = (pci_config_read(AC97_DEV, PCI_HEADER_BAR0) >> 8) & 0xffff;
-  _nabm_port = (pci_config_read(AC97_DEV, PCI_HEADER_BAR1) >> 8) & 0xffff;
+  _nam_port = pci_config_read(AC97_DEV, PCI_HEADER_BAR0) & 0xfffc;
+  _nabm_port = pci_config_read(AC97_DEV, PCI_HEADER_BAR1) & 0xfffc;
 
   outb(_nabm_port+AC97_NABM_GCR, AC97_RESET);
-  outw(_nam_port+AC97_NAM_RESET, AC97_RESET);
-
-  outw(_nam_port+AC97_NAM_MASTER_VOLUME, 0);
-  outw(_nam_port+AC97_NAM_PCM_VOLUME, 0x0808);
 
   outb(_nabm_port+AC97_NABM_TC, AC97_RESET);
   while((inb(_nabm_port+AC97_NABM_TC) & AC97_RESET)) {
     asm volatile ("nop");
   }
 
+  outw(_nam_port+AC97_NAM_RESET, AC97_RESET);
+  outw(_nam_port+AC97_NAM_MASTER_VOLUME, 0);
+  outw(_nam_port+AC97_NAM_PCM_VOLUME, 0x0808);
+
   outd(_nabm_port+AC97_NABM_BDBA, (uint32_t)_buffer_list);
+
   return true;
 }
 
