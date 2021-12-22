bd31d216906745769ea0547c202485908eac16ea
Michele Balistreri <michele@bitgamma.com>
2021-12-22 09:32:36 +0300

    make easy mode easier

--- maxit/src/ai.c
+++ maxit/src/ai.c
@@ -19,16 +19,24 @@ static bitarray_t _moves[BOARD_SIZE][1];
 #define WIN (PINF - 1)
 #define LOSE (MINF + 1)
 
-int16_t max(int16_t a, int16_t b) {
+static inline int16_t max(int16_t a, int16_t b) {
   return a > b ? a : b;
 }
 
-int16_t min(int16_t a, int16_t b) {
+static inline int16_t min(int16_t a, int16_t b) {
   return a < b ? a : b;
 }
 
 static inline bool coin() {
   return rnd_next() & 1;
+}
+
+static inline bool mxt_ai_should_mistake(mxt_maxit_t* maxit) {
+  if (maxit->game.ai_mode != EASY) {
+    return false;
+  }
+
+  return (rnd_next() % (maxit->game.level + 4)) == 0;
 }
 
 void mxt_ai_init(mxt_maxit_t* maxit) {
@@ -125,6 +133,14 @@ uint8_t mxt_ai_move(mxt_maxit_t* maxit) {
   uint8_t move;
   mxt_ai_minmaxi(maxit, maxit->game.board.cursor_column, (maxit->opponent.score - maxit->player.score), mxt_ai_get_depth(maxit), MINF, PINF, true, &move);
   ba_clear(_moves[move], maxit->game.board.cursor_column);
+
+  if (mxt_ai_should_mistake(maxit)) {
+    uint8_t move2 = move;
+    mxt_ai_minmaxi(maxit, maxit->game.board.cursor_column, (maxit->opponent.score - maxit->player.score), 1, MINF, PINF, true, &move2);
+    ba_set(_moves[move], maxit->game.board.cursor_column);
+    ba_clear(_moves[move2], maxit->game.board.cursor_column);
+    move = move2;
+  }
 
   return move;
 }
